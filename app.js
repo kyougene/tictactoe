9bb16fa504342ce682e17fa355f6a88d2421c8c9
Kai Eugene <kai.yougene@gmail.com>
2023-03-07 13:35:52 -0800

    restart button, choosing spot for x and o's

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const startButton = document.querySelector('#start-game');
-const resetButton = document.querySelector('#reset-game');
-
-startButton.addEventListener('click', ()=> {
-    Game.start();
-})
-
 const Gameboard = (()=> {
     let gameboard = ['', '', '', '', '', '', '', '', ''];
 
     const render = () => {
         let boardHTML = '';
         gameboard.forEach((square, index)=>{
-            boardHTML += `<div class='square' id='square-${index}'>${square}</div> `
+            boardHTML += `<div class='square' id='square-${index}'>${square}</div> `;
         })
-        document.querySelector("#gameboard").innerHTML = boardHTML;
+        document.querySelector("#gameboard").innerHTML = boardHTML; 
     }
+
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    const getGameboard = () => gameboard;
+
+
     return {
-        render
+        render,
+        update,
+        getGameboard
     }
 })();
 
@@ -40,8 +44,45 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            addEventListener('click', handleClick); 
+        })
     }
+
+    const handleClick = (e) => {
+        let index = parseInt(e.target.id.split('-')[1]);
+        if (Gameboard.getGameboard()[index] !== '') {
+            return;
+        }
+
+        Gameboard.update(index, players[currentPlayerIndex].mark); 
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++){
+            Gameboard.update(i, '');
+        }
+        Gameboard.render();
+    }
+
+
     return {
-        start
+        start,
+        handleClick,
+        restart
     }
 })()
+
+
+const startButton = document.querySelector('#start-game');
+const resetButton = document.querySelector('#reset-game');
+
+startButton.addEventListener('click', ()=> {
+    Game.start();
+})
+
+resetButton.addEventListener('click', ()=> {
+    Game.restart();
+})
