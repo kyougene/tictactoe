9140a1dc45a3af291e6be8a9e6ab7445beb2bded
Kai Eugene <kai.yougene@gmail.com>
2023-03-07 12:17:05 -0800

    rendering gameboard upon start button

---
+++ app.js
@@ -0,0 +1,47 @@
+const startButton = document.querySelector('#start-game');
+const resetButton = document.querySelector('#reset-game');
+
+startButton.addEventListener('click', ()=> {
+    Game.start();
+})
+
+const Gameboard = (()=> {
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+    const render = () => {
+        let boardHTML = '';
+        gameboard.forEach((square, index)=>{
+            boardHTML += `<div class='square' id='square-${index}'>${square}</div> `
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+    }
+    return {
+        render
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, 'X'), 
+            createPlayer(document.querySelector('#player2').value, 'O')
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+    return {
+        start
+    }
+})()
